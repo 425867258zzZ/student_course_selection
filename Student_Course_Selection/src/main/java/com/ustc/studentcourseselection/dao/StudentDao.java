114dd0422aa6bb77d84aea38541bacd8eb306242
Meng Zi Han <425867258@qq.com>
2024-05-17 19:11:07 +0800

    增加部分修改

--- Student_Course_Selection/src/main/java/com/ustc/studentcourseselection/dao/StudentDao.java
+++ Student_Course_Selection/src/main/java/com/ustc/studentcourseselection/dao/StudentDao.java
@@ -19,8 +19,8 @@ import java.util.List;
 public class StudentDao {
 
     private static boolean sqlDeal(Student student, Connection connection, String sql) {
+        PreparedStatement ps = null;
         try {
-            PreparedStatement ps = null;
             if (connection != null) {
                 ps = connection.prepareStatement(sql);
             }
@@ -44,25 +44,14 @@ public class StudentDao {
             }
             DBconnection.closeConnection(null, null, connection);
             return result;
-        } catch (NullPointerException e) {
-            System.out.println("NullPointerException");
-        } catch (SQLException e) {
-            System.out.println("SQLException");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            DBconnection.closeConnection(null, ps, connection);
         }
         return false;
     }
 
-    public static void main(String[] args) {
-        try {
-            BaseObject object = Factory.createModel(Model.Student, 4, "张国立", "PB23111645", "男", 2023, "本科", "数学", "1班", "123456", BaseUtils.getTime(), BaseUtils.getTime());
-            ObjectDao dao = (ObjectDao) Class.forName("com.ustc.studentcourseselection.dao." + Model.Student.name() + "Dao").getConstructor().newInstance();
-            dao.add(object);
-        } catch (RuntimeException | InstantiationException | IllegalAccessException | InvocationTargetException e) {
-            System.out.println("Creation failed");
-        } catch (ClassNotFoundException | NoSuchMethodException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
     /**
      * 添加学生
@@ -70,43 +59,11 @@ public class StudentDao {
      * @param baseObject 学生
      * @return 布尔值
      */
-
-
     public boolean add(BaseObject baseObject) {
         Student student = (Student) baseObject;
         Connection connection = DBconnection.getConnection();
         String sql1 = "INSERT INTO student(id,name,number,gender,grade,degree,major,className,password,createTime,updateTime) VALUES(?,?,?,?,?,?,?,?,?,?,?)";
-        try {
-            PreparedStatement ps = null;
-            if (connection != null) {
-                ps = connection.prepareStatement(sql1);
-            }
-            if (ps != null) {
-                ps.setString(1, student.getName());
-                ps.setString(2, student.getNumber());
-                ps.setString(3, student.getGender());
-                ps.setInt(5, student.getGrade());
-                ps.setString(5, student.getDegree());
-                ps.setString(6, student.getMajor());
-                ps.setString(7, student.getClassName());
-                ps.setString(8, student.getPassword());
-                ps.setString(9, student.getCreateTime());
-                ps.setString(10, student.getUpdateTime());
-                ps.setInt(11, student.getId());
-            }
-
-            boolean result = false;
-            if (ps != null) {
-                result = ps.executeUpdate() > 0;
-            }
-            DBconnection.closeConnection(null, null, connection);
-            return result;
-        } catch (NullPointerException e) {
-            System.out.println("NullPointerException");
-        } catch (SQLException e) {
-            System.out.println("SQLException");
-        }
-        return false;
+        return sqlDeal(student, connection, sql1);
     }
 
     /**
@@ -119,8 +76,8 @@ public class StudentDao {
     public boolean del(String number) {
         Connection connection = DBconnection.getConnection();
         String sql2 = "DELETE FROM student WHERE number = ?";
+        PreparedStatement ps = null;
         try {
-            PreparedStatement ps = null;
             if (connection != null) {
                 ps = connection.prepareStatement(sql2);
             }
@@ -133,26 +90,25 @@ public class StudentDao {
             }
             DBconnection.closeConnection(null, ps, connection);
             return result;
-        } catch (NullPointerException e) {
-            System.out.println("NullPointerException");
-        } catch (SQLException e) {
-            System.out.println("SQLException");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            DBconnection.closeConnection(null, ps, connection);
         }
         return false;
     }
 
     /**
      * 修改学生数据
-     * TODO 完善sql语句和处理逻辑
      *
      * @param student 学生
      * @return 布尔值
      */
     public boolean update(Student student) {
         Connection connection = DBconnection.getConnection();
-        String sql = "UPDATE student SET name=?, number=?, gender=?, grade=?, degree=?, major=?, className=?, password=?, updateTime=? WHERE id=?";
+        String sql = "UPDATE student SET name=?, number=?, gender=?, grade=?, degree=?, major=?, className=?, updateTime=? WHERE id=?";
+        PreparedStatement ps = null;
         try {
-            PreparedStatement ps = null;
             if (connection != null) {
                 ps = connection.prepareStatement(sql);
             }
@@ -164,21 +120,16 @@ public class StudentDao {
                 ps.setString(5, student.getDegree());
                 ps.setString(6, student.getMajor());
                 ps.setString(7, student.getClassName());
-                ps.setString(8, student.getPassword());
-                ps.setString(9, BaseUtils.getTime());
-                ps.setInt(10, student.getId());
+                ps.setString(8, BaseUtils.getTime());
+                ps.setInt(9, student.getId());
             }
-
-            boolean result = false;
             if (ps != null) {
-                result = ps.executeUpdate() > 0;
+                return ps.executeUpdate() > 0;
             }
-            DBconnection.closeConnection(null, null, connection);
-            return result;
-        } catch (NullPointerException e) {
-            System.out.println("NullPointerException");
-        } catch (SQLException e) {
-            System.out.println("SQLException");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            DBconnection.closeConnection(null, ps, connection);
         }
         return false;
     }
@@ -194,8 +145,8 @@ public class StudentDao {
     public Student query(String number) {
         Connection connection = DBconnection.getConnection();
         String sql2 = "SELECT * FROM student WHERE number = ?";
+        PreparedStatement ps = null;
         try {
-            PreparedStatement ps = null;
             if (connection != null) {
                 ps = connection.prepareStatement(sql2);
             }
@@ -213,6 +164,8 @@ public class StudentDao {
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
+        } finally {
+            DBconnection.closeConnection(null, ps, connection);
         }
         return null;
     }
@@ -226,28 +179,27 @@ public class StudentDao {
     public List<Student> queryAll() {
         Connection connection = DBconnection.getConnection();
         String sql2 = "SELECT * FROM student";
+        ResultSet rs = null;
+        PreparedStatement ps = null;
         try {
-            PreparedStatement ps = null;
             if (connection != null) {
                 ps = connection.prepareStatement(sql2);
             }
-            ResultSet rs = null;
             if (ps != null) {
                 rs = ps.executeQuery();
             }
             List<Student> students = new ArrayList<>();
             if (rs != null) {
                 while (rs.next()) {
-                    Student student = new Student(rs.getInt("id"), rs.getString("name"), rs.getString("number"), rs.getString("gender"),
-                            rs.getInt("grade"), rs.getString("degree"), rs.getString("major"), rs.getString("className"),
-                            rs.getString("password"), rs.getString("createTime"), rs.getString("updateTime"));
+                    Student student = new Student(rs.getInt("id"), rs.getString("name"), rs.getString("number"), rs.getString("gender"), rs.getInt("grade"), rs.getString("degree"), rs.getString("major"), rs.getString("className"), rs.getString("password"), rs.getString("createTime"), rs.getString("updateTime"));
                     students.add(student);
                 }
             }
-            DBconnection.closeConnection(rs, null, connection);
             return students;
         } catch (Exception e) {
             System.out.println(e.getMessage());
+        } finally {
+            DBconnection.closeConnection(rs, ps, connection);
         }
         return null;
     }
